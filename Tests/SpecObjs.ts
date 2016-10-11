b4d3cf9bc5c365a9c2e434ceaeb207c73985eb35
Matthew Thomas Donovan <matthewthomasdonovan@gmail.com>
2016-10-11 11:52:05 -0600

    Update SpecObjs.ts

--- Tests/SpecObjs.ts
+++ Tests/SpecObjs.ts
@@ -9,11 +9,6 @@ var learn: any = header.element(by.css('li:nth-child(4) > a[href]'));
 var develop: any = header.element(by.css('li:nth-child(6) > a[href]'));
 
 class NavBar{
-    // header: any = element(by.css('div[class="navbar-inner"]'));
-    // home: any = this.header.element(by.css('li.active > a[href]'));
-    // learn: any = this.header.element(by.css('li:nth-child(4) > a[href]'));
-    // develop: any = this.header.element(by.css('li:nth-child(6) > a[href]'));
-
     private goto(selector){
         browser.wait(ec.elementToBeClickable(selector), 2000).then(() => {
             selector.click();
@@ -21,27 +16,6 @@ class NavBar{
             browser.sleep(300);
         });
     }
-
-//     public gotoHome(){
-//         var navBar: any = new NavBar();
-//
-//         navBar.goto(this.home);
-//         return this;
-//     }
-//     public gotoLearn(){
-//         var navBar: any = new NavBar();
-//
-//         navBar.goto(this.learn);
-//         return this;
-//     }
-//     public gotoDevelop(){
-//         var navBar: any = new NavBar();
-//
-//         navBar.goto(this.develop);
-//         return this;
-//     }
-// }
-
     public gotoHome(){
         var navBar: any = new NavBar();
 

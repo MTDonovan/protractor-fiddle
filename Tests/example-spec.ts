3e3ab49e8fe4af7d8c198891be1531dfa172fb37
Matthew Thomas Donovan <matthewthomasdonovan@gmail.com>
2017-01-28 13:11:40 -0700

    Update example-spec.ts

--- Tests/example-spec.ts
+++ Tests/example-spec.ts
@@ -18,13 +18,8 @@ describe('angularjs homepage', () => {
 
     it('Click the "Home" link in the site\'s header', () => {
         var header: any = element(by.css('div[class="navbar-inner"]'));
-<<<<<<< HEAD
         browser.wait(EC.visibilityOf(header), 10000).then(() => {
           navBar
-=======
-        browser.wait(ec.visibilityOf(header), 10000).then(() => {
-        	navBar
->>>>>>> 55182d76e09a85657ee1329ad334436834a90c15
 		    .goto('Learn')
 		    .goto('Develop')
 		    .goto('Discuss');

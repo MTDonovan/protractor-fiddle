8a039e115a82b8f0dc0b3c837b443b2fa2bd8a31
Matthew Thomas Donovan <matthewthomasdonovan@gmail.com>
2019-02-03 15:56:37 -0700

    Update SpecObjs.ts

--- Tests/SpecObjs.ts
+++ Tests/SpecObjs.ts
@@ -1,11 +1,11 @@
 /// <reference path="../testheader.ts" />
 const EC: any = protractor.ExpectedConditions;
 
-class NavBar{
-    public goto(selector){
+class NavBar {
+    public goto(selector) {
       var header_selector: any = element(by.xpath('//a[@href][@class="dropdown-toggle"][text()=" '+ selector +' "]'));
 
-      function gotoSelector(){
+      function gotoSelector() {
         browser.wait(EC.elementToBeClickable(header_selector), 5000).then(() => {
             header_selector.click();
         });
